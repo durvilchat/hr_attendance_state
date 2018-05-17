9a09e085eb0c28818146aaf8604dcc4cd180ccf9
durvilchat <rostapig@gmail.com>
2018-05-17 20:05:53 +0100

    Add files via upload

---
+++ hr_attendance_state/__manifest__.py
@@ -0,0 +1,17 @@
+{
+    "name": "Attendance state",
+    "summary": "Attendance state",
+    "category": "Human Resources",
+    "version": "1.0",
+    'author': "Kywana dev solution",
+    "depends": ['hr_attendance'],
+    "data": [
+        'views/hr_attendance.xml',
+    ],
+    'qweb': ['static/src/xml/attendance.xml'],
+    "installable": True,
+    "auto_install": False,
+    "price": 4,
+    "currency": "EUR",
+    'license': 'AGPL-3'
+}
